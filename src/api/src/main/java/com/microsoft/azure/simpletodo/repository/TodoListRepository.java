1915eccdd8b2b67c7163929056267b477a61ad2a
azure-sdk <azuresdk@microsoft.com>
2022-09-22 00:55:45 +0000

    Synchronize repo from Repoman

--- src/api/src/main/java/com/microsoft/azure/simpletodo/repository/TodoListRepository.java
+++
@@ -1,7 +0,0 @@
-package com.microsoft.azure.simpletodo.repository;
-
-import com.microsoft.azure.simpletodo.model.TodoList;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface TodoListRepository extends MongoRepository<TodoList, String> {
-}
